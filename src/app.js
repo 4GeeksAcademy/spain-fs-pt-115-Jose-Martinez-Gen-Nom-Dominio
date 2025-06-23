ef2f6a1589decafc93ac8709feb9fac15b4b68ce
Jose <avu11@protonmail.com>
2025-06-23 09:45:57 +0000

    Terminado el ejercicio

--- src/app.js
+++ src/app.js
@@ -9,3 +9,19 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+//inicio aqui 
+let pronouns = ['the', 'our'];
+let adjs = ['great', 'big'];
+let nouns = ['jogger', 'racoon'];
+
+console.log("Generando nombres de dominio (versión moderna):");
+
+for (const pronoun of pronouns) {
+  for (const adj of adjs) {
+    for (const noun of nouns) {
+     
+      console.log(`${pronoun}${adj}${noun}.com`);
+    }
+  }
+}
